e904577696cad6059bd0b3ffa68ba29d2389829a
Lucio Bianchi <luciobianchipradas@gmail.com>
2025-08-18 20:33:44 -0300

    El texto del inicio es accesible por teclado

--- frontend/src/components/WelcomeScreen/WelcomeScreen.jsx
+++ frontend/src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -16,22 +16,33 @@ export default function WelcomeScreen({ onStart }) {
       aria-modal="true"
       aria-labelledby="welcome-title"
       aria-describedby="welcome-desc"
-      tabIndex={-1}
+      tabIndex={0}
     >
-      <div className="welcome-overlay">
-        <div className="welcome-centerbox">
-          <h1 id="welcome-title" className="welcome-title">
+      <div className="welcome-overlay" tabIndex={0}>
+        <div className="welcome-centerbox" tabIndex={0}>
+          <h1 
+            id="welcome-title" 
+            className="welcome-title"
+            tabIndex={0}
+            aria-label="Rio x Todos"
+          >
             Rio x Todos
           </h1>
-          <p id="welcome-desc" className="welcome-desc">
-          Conocé el nivel de contaminación de los balnearios del Rio de La Plata de forma rápida y confiable. Además, investigadores y usuarios habilitados pueden colaborar cargando mediciones para construir una base científica abierta y dinámica.
+          <p 
+            id="welcome-desc" 
+            className="welcome-desc"
+            tabIndex={0}
+            aria-label="Conocé el nivel de contaminación de los balnearios del Rio de La Plata de forma rápida y confiable. Además, investigadores y usuarios habilitados pueden colaborar cargando medición para construir una base científica abierta y dinámica."
+          >
+            Conocé el nivel de contaminación de los balnearios del Rio de La Plata de forma rápida y confiable. Además, investigadores y usuarios habilitados pueden colaborar cargando medición para construir una base científica abierta y dinámica.
           </p>
           <button
             className="welcome-start-btn"
             onClick={onStart}
             ref={startBtnRef}
-            aria-label="Comenzar a usar la aplicación"
+            aria-label="Comenzar"
             type="button"
+            tabIndex={0}
           >
             Comenzar
           </button>
